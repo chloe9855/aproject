7aa63e9e674189f168fcc53eb03be685ef7a6a54
chloe9855 <liu870707@gmail.com>
2021-09-08 15:48:05 +0800

    [新增]AlertBox.vue

---
+++ components/tools/AlertBox.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div class="modal">
+      <img
+        class="close"
+        :src="require('~/assets/img/close-icon.svg')"
+      >
+      <img
+        class="icon"
+        :src="require('~/assets/img/alert-icon.svg')"
+      >
+      <p class="title">
+        小標題文字文字文字
+      </p>
+      <p class="text">
+        警示文字警示文字警示文字警示文字警示文字警示文字警示文字警示文字警示文字
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="scss" scoped>
+
+  .modal {
+    width: 350px;
+  }
+
+</style>
