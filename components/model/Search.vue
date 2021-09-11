07c667e71e3dacfbf1769f43d802a669cb0ba1ae
chingHsiang <chinghsiang15@gmail.com>
2021-09-11 09:56:23 +0800

    [新增]Search組件(Search)

---
+++ components/model/Search.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="searchBox">
+    <div class="switchTab">
+      SWITCH
+    </div>
+    <div class="searchInput">
+      INPUT
+    </div>
+    <div class="searchBtn">
+      BUTTON
+    </div>
+    <div class="toggleSearch">
+      TOGGLE
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Table',
+  props: {
+    tableColumn: {
+      type: Object,
+      default: () => {
+        return {
+          searchData: {}
+        };
+      }
+    }
+  },
+  computed: {},
+  created: function () {
+
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
