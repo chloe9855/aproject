a620807103a8297bfc9144b77efb8131d671581d
chingHsiang <chinghsiang15@gmail.com>
2021-09-17 17:26:31 +0800

    [新增]Popup(Popup組件,Mask組件,內容區塊)

---
+++ components/model/popup/edit.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="inputBox">
+    <InputVertical title="輸入框標題1" />
+    <InputVertical title="輸入框標題1" />
+    <InputVertical title="輸入框標題1" />
+    <InputVertical title="輸入框標題1" />
+    <InputVertical title="輸入框標題1" />
+    <div class="buttonBox">
+      <Button
+        :name="'button-primary'"
+        :text="'按鈕'"
+      />
+      <Button
+        :name="'button-default-disable'"
+        :text="'按鈕'"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import InputVertical from '~/components/tools/InputVertical.vue';
+import Button from '~/components/tools/Buttons.vue';
+export default {
+  components: {
+    InputVertical: InputVertical,
+    Button: Button
+  },
+  data: () => {
+    return {};
+  },
+  name: 'LandSearch',
+  computed: {}
+};
+</script>
+
+<style lang="scss" scoped>
+.inputBox{
+  padding: 1em;
+}
+.buttonBox{
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 20px;
+}
+</style>
---
+++ components/model/popup/tableData.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="inputBox">
+    <InputVertical title="表單名稱" />
+    <Buttons-component
+      :name="'button-round'"
+      :text="'新增檔案'"
+      :round="true"
+    />
+    <Table />
+    <div class="buttonBox">
+      <Button
+        :name="'button-primary'"
+        :text="'按鈕'"
+      />
+      <Button
+        :name="'button-default-disable'"
+        :text="'按鈕'"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import InputVertical from '~/components/tools/InputVertical.vue';
+import Button from '~/components/tools/Buttons.vue';
+import Table from '~/components/tools/Table.vue';
+export default {
+  components: {
+    InputVertical: InputVertical,
+    Button: Button,
+    Table: Table
+  },
+  data: () => {
+    return {};
+  },
+  name: 'TableData',
+  computed: {}
+};
+</script>
+
+<style lang="scss" scoped>
+.inputBox{
+  padding: 1em;
+}
+.buttonBox{
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 20px;
+}
+</style>
