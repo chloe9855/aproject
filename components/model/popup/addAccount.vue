fca4517a68620d151bc98b37f205a518ea4aceb3
chingHsiang <chinghsiang15@gmail.com>
2021-09-30 16:11:31 +0800

    [新增] Popup (  Popup 新增Popup Box )

--- components/model/popup/addAccount.vue
+++ components/model/popup/addAccount.vue
@@ -2,37 +2,12 @@
   <div class="inputBox theme_scrollbar">
     <InputVertical title="姓名" />
     <InputVertical title="帳號" />
-    <div
-      v-show="!onPassword"
-      class="box"
-    >
-      <span>密碼</span>
-      <Button
-        text="重設密碼"
-        name="button-red"
-        @click="restorePassword"
-      />
-    </div>
-    <InputVertical
-      v-show="onPassword"
-      is-icon="lock"
-      title="請輸入密碼"
-    />
-    <InputVertical
-      v-show="onPassword"
-      is-icon="lock"
-      title="請再次輸入密碼"
-    />
     <DropdownVertical title="單位" />
     <DropdownVertical title="工作站" />
     <DropdownVertical title="小組別" />
     <InputVertical title="信箱" />
     <InputVertical title="備註" />
     <DropdownVertical title="群組" />
-    <div class="box">
-      <span>帳號啟用</span>
-      <Tag text="驗證中" />
-    </div>
     <div class="buttonBox">
       <Button
         :name="'button-primary'"
@@ -50,20 +25,18 @@
 import InputVertical from '~/components/tools/InputVertical.vue';
 import DropdownVertical from '~/components/tools/DropdownVertical.vue';
 import Button from '~/components/tools/Buttons.vue';
-import Tag from '~/components/tools/Tag.vue';
 export default {
   components: {
     InputVertical,
     DropdownVertical,
-    Button,
-    Tag
+    Button
   },
   data: () => {
     return {
       onPassword: false
     };
   },
-  name: 'EditAccount',
+  name: 'AddAccount',
   methods: {
     restorePassword () {
       this.onPassword = !this.onPassword;
---
+++ components/model/popup/addTableData.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="inputBox theme_scrollbar">
+    <InputVertical title="表單名稱" />
+    <div
+      class="box"
+    >
+      <Button
+        text="新增檔案"
+        name="button-red"
+        :add="true"
+      />
+    </div>
+    <Table
+      :table-column="tableList"
+      :is-paginate="false"
+      :is-del="true"
+    />
+    <div class="buttonBox">
+      <Button
+        :name="'button-default-disable'"
+        :text="'取消'"
+      />
+      <Button
+        :name="'button-primary'"
+        :text="'新增'"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import InputVertical from '~/components/tools/InputVertical.vue';
+import Button from '~/components/tools/Buttons.vue';
+import Table from '~/components/model/Table.vue';
+export default {
+  components: {
+    InputVertical,
+    Button,
+    Table
+  },
+  data: () => {
+    return {
+      tableList: {
+        head: [
+          { title: '檔案名稱' },
+          { title: '檔案' }
+        ],
+        body: [
+          { val: 0, title: [{ type: 'input' }, { type: 'btn' }] },
+          { val: 1, title: [{ type: 'input' }, { type: 'btn' }] },
+          { val: 2, title: [{ type: 'input' }, { type: 'btn' }] },
+          { val: 3, title: [{ type: 'input' }, { type: 'btn' }] }
+        ]
+      }
+    };
+  },
+  name: 'AddTableData'
+};
+</script>
+
+<style lang="scss" scoped>
+.inputBox{
+  padding: 1em;
+}
+.buttonBox{
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 20px;
+}
+.box{
+  margin: 0.5em auto;
+}
+</style>
