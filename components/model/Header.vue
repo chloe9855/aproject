c712918dd033d11c88a9cc2e69ab0fecd7785feb
chloe9855 <liu870707@gmail.com>
2021-10-27 17:15:20 +0800

    [修改] Header順序 [新增] OGC介接/底圖切換

--- components/model/Header.vue
+++ components/model/Header.vue
@@ -142,16 +142,16 @@ export default {
           path: ''
         },
         {
-          name: '作業基金土地管理',
-          path: 'fundLand'
+          name: '灌溉地管理',
+          path: 'irrigatedLand'
         },
         {
-          name: 'GIS地圖',
+          name: '地理資訊圖台',
           path: 'map'
         },
         {
-          name: '灌溉地管理',
-          path: 'irrigatedLand'
+          name: '作業基金土地管理',
+          path: 'fundLand'
         },
         {
           name: '系統管理',
