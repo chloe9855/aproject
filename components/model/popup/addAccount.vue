31f5a5bebb9dcd7dd035532e5b28dd9739dc24ce
chingHsiang <chinghsiang15@gmail.com>
2021-11-09 01:43:25 +0800

    [修改]新增帳號取得DATA

--- components/model/popup/addAccount.vue
+++ components/model/popup/addAccount.vue
@@ -1,13 +1,37 @@
 <template>
   <div class="inputBox theme_scrollbar">
-    <InputVertical title="姓名" />
-    <InputVertical title="帳號" />
-    <DropdownVertical title="單位" />
-    <DropdownVertical title="工作站" />
-    <DropdownVertical title="小組別" />
-    <InputVertical title="信箱" />
-    <InputVertical title="備註" />
-    <DropdownVertical title="群組" />
+    <InputVertical
+      title="姓名"
+      @inputValue="setName"
+    />
+    <InputVertical
+      title="帳號"
+      @inputValue="setAccount"
+    />
+    <DropdownVertical
+      title="單位"
+      @DropdownVal="setUnit"
+    />
+    <DropdownVertical
+      title="工作站"
+      @DropdownVal="setSite"
+    />
+    <DropdownVertical
+      title="小組別"
+      @DropdownVal="setGrp"
+    />
+    <InputVertical
+      title="信箱"
+      @inputValue="setEmail"
+    />
+    <InputVertical
+      title="備註"
+      @inputValue="setText"
+    />
+    <DropdownVertical
+      title="群組"
+      @DropdownVal="setGroup"
+    />
   </div>
 </template>
 
@@ -21,13 +45,58 @@ export default {
   },
   data: () => {
     return {
-      onPassword: false
+      onPassword: false,
+      accountData: {
+        name: '',
+        account: '',
+        unit: '',
+        site: '',
+        grp: '',
+        email: '',
+        text: '',
+        group: ''
+      }
     };
   },
   name: 'AddAccount',
   methods: {
     restorePassword () {
       this.onPassword = !this.onPassword;
+    },
+    setName (e) {
+      if (e) {
+        this.accountData.name = e;
+      }
+    },
+    setAccount (e) {
+      if (e) {
+        this.accountData.account = e;
+      }
+    },
+    setUnit (e) {
+      if (e) {
+        this.accountData.unit = e;
+      }
+    },
+    setSite (e) {
+      if (e) {
+        this.accountData.site = e;
+      }
+    },
+    setGrp (e) {
+      if (e) {
+        this.accountData.grp = e;
+      }
+    },
+    setEmail (e) {
+      if (e) {
+        this.accountData.email = e;
+      }
+    },
+    setGroup (e) {
+      if (e) {
+        this.accountData.group = e;
+      }
     }
   }
 };
