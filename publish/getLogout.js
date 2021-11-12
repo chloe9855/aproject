f5d169a2aa3f6d6f26f30140a5035abe1c0a0983
chingHsiang <chinghsiang15@gmail.com>
2021-11-12 08:54:33 +0800

    [修改] 登出呼叫

--- publish/getLogout.js
+++ publish/getLogout.js
@@ -1,12 +1,15 @@
 import { logout } from '~/api/login';
 export function getLogout (t) {
   logout().then((r) => {
-    console.log(r);
-    t.$store.commit('SET_USER_INFO', { userInfo: {} });
+    // t.$store.commit('SET_USER_INFO', { userInfo: {} });
     sessionStorage.setItem('loginStatus', 0);
-    t.$cookies.set('ASP.NET_SessionId', '');
+    // t.$cookies.set('ASP.NET_SessionId', '');
     t.$router.push('/login');
   }).catch((e) => {
     console.log(e);
+    console.log(e.response.status);
+    if (e.response.status === '401') {
+      t.$router.push('/login');
+    }
   });
 }
